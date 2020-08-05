c80167025c55113a08c8338673ad646a47dd5863
Aleksandr Mirskoy <almirskoy@avito.ru>
2020-08-05 13:50:08 +0300

    add csv and update evnironment variables

--- docker-entrypoint-initdb.d/03_fill_tables.sql
+++ docker-entrypoint-initdb.d/03_fill_tables.sql
@@ -1,5 +1,5 @@
--- COPY users_evaluation_of_satisfaction FROM '/pg_init_data/users_evaluation_of_satisfaction.csv' DELIMITER ',' QUOTE '"' CSV HEADER WHERE request_id IS NOT NULL;
+COPY users_evaluation_of_satisfaction FROM '/pg_init_data/users_evaluation_of_satisfaction.csv' DELIMITER ',' QUOTE '"' CSV HEADER WHERE request_id IS NOT NULL;
 
--- COPY support_tickets FROM '/pg_init_data/support_tickets.csv' DELIMITER ',' QUOTE '"' CSV HEADER;
+COPY support_tickets FROM '/pg_init_data/support_tickets.csv' DELIMITER ',' QUOTE '"' CSV HEADER;
 
--- COPY new_items_by_support_users FROM '/pg_init_data/new_items_by_support_users_proc.csv' DELIMITER ',' QUOTE '"' CSV HEADER;
+COPY new_items_by_support_users FROM '/pg_init_data/new_items_by_support_users.csv' DELIMITER ',' QUOTE '"' CSV HEADER;
